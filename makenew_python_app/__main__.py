554f1408ce3aa2b3a53cc728146fa0dd67ae443d
Evan Sosenko <razorx@evansosenko.com>
2020-03-29 15:14:59 -0700

    Set event loop option for Windows

--- makenew_python_app/__main__.py
+++ makenew_python_app/__main__.py
@@ -1,7 +1,13 @@
+import asyncio
+import platform
+
 from . import create_dependencies, boot
 
 def main():
     boot(create_dependencies)
 
 if __name__ == "__main__":
+    if (platform.system() == 'Windows'):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
     main()
